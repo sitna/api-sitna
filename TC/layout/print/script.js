5760ff5e2cc70775281a751f182cfabb922b23a5
Fernando Lacunza <flacunza@users.noreply.github.com>
2017-02-24 13:46:03 +0100

    Add print layout

---
+++ TC/layout/print/script.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+
+    var size = TC.Util.getParameterByName("size");
+    var orientation = TC.Util.getParameterByName("orientation");
+    var title = TC.Util.getParameterByName("title");
+
+    $("body").addClass("tc-print-" + orientation + "-" + size);
+
+    if ($('head > style').length === 0) {
+        $("head").append('<style></style>');
+    }
+
+    document.querySelector('style').textContent += "@media print { @page {size: " + size + " " + orientation + "; margin: 10mm 10mm 10mm 10mm;}}";
+
+    if (title) {
+        window.document.title = title;
+    }
+});
