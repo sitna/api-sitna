38cbb342177c2a20c5abb7a943144dffa5e085ce
flacunzap <flacunza@gmail.com>
2018-01-23 10:03:55 +0100

    Set attribution links from capabilities

--- TC/control/Attribution.js
+++ TC/control/Attribution.js
@@ -11,9 +11,10 @@ TC.control.Attribution = function () {
 
     self.apiAttribution = '';
     self.mainDataAttribution = '';
-    self.dataAttributions = self.options.dataAttributions && self.options.dataAttributions instanceof Array ?
-        self.options.dataAttributions : [self.options.dataAttributions] ||
-        [];
+    self.dataAttributions = [];
+    if (self.options.dataAttributions) {
+        self.dataAttributions = self.options.dataAttributions instanceof Array ? self.options.dataAttributions : [self.options.dataAttributions];
+    }
 };
 
 TC.inherit(TC.control.Attribution, TC.Control);
@@ -27,7 +28,7 @@ TC.inherit(TC.control.Attribution, TC.Control);
         ctlProto.template = TC.apiLocation + "TC/templates/Attribution.html";
     }
     else {
-        ctlProto.template = function () { dust.register(ctlProto.CLASS, body_0); function body_0(chk, ctx) { return chk.w("<div><span>&copy; ").f(ctx.get(["api"], false), ctx, "h", ["s"]).w("</span> - ").h("i18n", ctx, {}, { "$key": "data" }).w(": <span>").f(ctx.get(["mainData"], false), ctx, "h", ["s"]).w("</span>").x(ctx.get(["otherData"], false), ctx, { "block": body_1 }, {}).w("<div class=\"tc-ctl-attrib-other tc-collapsed\">").s(ctx.get(["otherData"], false), ctx, { "block": body_2 }, {}).w("</div></div>"); } body_0.__dustBody = !0; function body_1(chk, ctx) { return chk.w(" - <span class=\"tc-ctl-attrib-cmd\">").h("i18n", ctx, {}, { "$key": "others" }).w("...</span>"); } body_1.__dustBody = !0; function body_2(chk, ctx) { return chk.w("<span>").f(ctx.getPath(false, ["otherData", ctx.get(["$idx"], false)]), ctx, "h", ["s"]).w("</span>").h("sep", ctx, { "block": body_3 }, {}); } body_2.__dustBody = !0; function body_3(chk, ctx) { return chk.w(", "); } body_3.__dustBody = !0; return body_0 };
+        ctlProto.template = function () { dust.register(ctlProto.CLASS, body_0); function body_0(chk, ctx) { return chk.w("<div><span>&copy; ").f(ctx.get(["api"], false), ctx, "h", ["s"]).w("</span> - ").h("i18n", ctx, {}, { "$key": "data" }).w(": ").x(ctx.getPath(false, ["mainData", "site"]), ctx, { "else": body_1, "block": body_2 }, {}).x(ctx.get(["otherData"], false), ctx, { "block": body_3 }, {}).w("<div class=\"tc-ctl-attrib-other tc-collapsed\">").s(ctx.get(["otherData"], false), ctx, { "block": body_4 }, {}).w("</div></div>"); } body_0.__dustBody = !0; function body_1(chk, ctx) { return chk.w("<span>&copy; ").f(ctx.getPath(false, ["mainData", "name"]), ctx, "h").w("</span>"); } body_1.__dustBody = !0; function body_2(chk, ctx) { return chk.w("<span>&copy; <a href=\"").f(ctx.getPath(false, ["mainData", "site"]), ctx, "h").w("\" target=\"_blank\">").f(ctx.getPath(false, ["mainData", "name"]), ctx, "h").w("</a></span>"); } body_2.__dustBody = !0; function body_3(chk, ctx) { return chk.w(" - <span class=\"tc-ctl-attrib-cmd\">").h("i18n", ctx, {}, { "$key": "others" }).w("...</span>"); } body_3.__dustBody = !0; function body_4(chk, ctx) { return chk.x(ctx.getPath(false, ["otherData", ctx.get(["$idx"], false), "site"]), ctx, { "else": body_5, "block": body_6 }, {}).h("sep", ctx, { "block": body_7 }, {}); } body_4.__dustBody = !0; function body_5(chk, ctx) { return chk.w("<span>&copy; ").f(ctx.getPath(false, ["otherData", ctx.get(["$idx"], false), "name"]), ctx, "h").w("</span>"); } body_5.__dustBody = !0; function body_6(chk, ctx) { return chk.w("<span>&copy; <a href=\"").f(ctx.getPath(false, ["otherData", ctx.get(["$idx"], false), "site"]), ctx, "h").w("\" target=\"_blank\">").f(ctx.getPath(false, ["otherData", ctx.get(["$idx"], false), "name"]), ctx, "h").w("</a></span>"); } body_6.__dustBody = !0; function body_7(chk, ctx) { return chk.w(", "); } body_7.__dustBody = !0; return body_0 };
     }
 
     ctlProto.register = function (map) {
@@ -38,15 +39,19 @@ TC.inherit(TC.control.Attribution, TC.Control);
 
         var addData = function (layer) {
             if (layer) {
-                var attr = $("<div/>").html($.trim(layer.wrap.getAttribution(TC.capabilities[layer.url]))).text();
+                // TODO: sanitizer
+                var attr = layer.wrap.getAttribution(TC.capabilities[layer.url]);
                 if (attr) {
-                    if (/IDENA/.test(attr)) {
-                        self.mainDataAttribution = '<a href="http://idena.navarra.es/" target="_blank">IDENA</a>';
+                    if (/IDENA/.test(attr.name)) {
+                        self.mainDataAttribution = {
+                            name: 'IDENA',
+                            site: 'http://idena.navarra.es/'
+                        };
                     }
                     else {
                         var textExists = false;
                         for (var i = 0; i < self.dataAttributions.length; i++) {
-                            if (attr === self.dataAttributions[i]) {
+                            if (attr.name === self.dataAttributions[i].name) {
                                 textExists = true;
                                 break;
                             }
@@ -77,10 +82,16 @@ TC.inherit(TC.control.Attribution, TC.Control);
                 };
 
                 if (checkRemoveData()) {
-                    var attr = $("<div/>").html($.trim(layer.wrap.getAttribution(TC.capabilities[layer.url]))).text();
+                    // TODO: sanitizer
+                    var attr = layer.wrap.getAttribution(TC.capabilities[layer.url]);
 
                     if (attr) {
-                        var index = self.dataAttributions.indexOf(attr);
+                        var index = self.dataAttributions.reduce(function (prev, cur, idx) {
+                            if (cur.name === attr.name) {
+                                return idx;
+                            }
+                            return prev;
+                        }, -1);
                         if (index > -1) {
                             self.dataAttributions.splice(index, 1);
                         }
