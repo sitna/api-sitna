73697491989954aedacf590eedd8e6f36c4891bd
flacunzap <flacunza@gmail.com>
2018-04-12 12:06:01 +0200

    Add getElevationGain function, add profile options

--- TC/tool/Elevation.js
+++ TC/tool/Elevation.js
@@ -8,12 +8,88 @@ TC.tool.Elevation = function (options) {
     self.minimumElevation = self.options.minimumElevation || -9998;
 };
 
+TC.tool.Elevation.getElevationGain = function (options) {
+    options = options || {};
+    const coords = options.coords;
+    if (coords && coords.length > 0 && coords[0].length > 2) { // si tenemos la Z
+        var uphill = 0;
+        var downhill = 0;
+        const hillDeltaThreshold = options.hillDeltaThreshold || 0;
+
+        var previousHeight;
+        var sectorMinHeight;
+        var sectorMaxHeight;
+        var previousUphill = true;
+
+        for (var c = 0; c < coords.length; c++) {
+            var point = coords[c];
+            var height = point[2];
+            if (height !== null) {
+                if (previousHeight === undefined) //--inicializar
+                {
+                    previousHeight = height;
+                    sectorMinHeight = height;
+                    sectorMaxHeight = height;
+                }
+
+                sectorMinHeight = Math.min(sectorMinHeight, height); //--actualizar mínimo y máximo del sector
+                sectorMaxHeight = Math.max(sectorMaxHeight, height);
+
+                var delta = height - previousHeight; //--calcular desnivel del punto respecto al anterior
+                // hillDeltaThreshold: altura de los dientes a despreciar
+                if (delta > hillDeltaThreshold || (delta > 0 && c == coords.length - 1)) //--Si se sube más del filtro (o se acaba el segmento subiendo)
+                {
+                    if (previousUphill) //--Si en el segmento anterior también se subía, incrementamos el desnivel positivo acumulado
+                    {
+                        uphill += delta;
+                    }
+                    else //--Si en el segmento anterior se bajaba, incrementamos los desniveles acumulados que no habíamos contabilizado desde el último salto del filtro (sector) 
+                    {
+                        downhill -= sectorMinHeight - previousHeight;
+                        uphill += height - sectorMinHeight;
+                        previousUphill = true; //--preparar para el paso siguiente
+                    }
+                    previousHeight = height; //--preparar para el paso siguiente
+                    sectorMinHeight = height;
+                    sectorMaxHeight = height;
+                }
+                else if (delta < -hillDeltaThreshold || (delta < 0 && c == coords.length - 1)) //--Si se baja más del filtro (o se acaba el segmento bajando)
+                {
+                    if (!previousUphill) //--Si en el segmento anterior también se bajaba, incrementamos el desnivel negativo acumulado
+                    {
+                        downhill -= delta;
+                    }
+                    else //--Si en el segmento anterior se subía, incrementamos los desniveles acumulados que no habíamos contabilizado desde el último salto del filtro (sector) 
+                    {
+                        uphill += sectorMaxHeight - previousHeight;
+                        downhill -= height - sectorMaxHeight;
+                        previousUphill = false; //--preparar para el paso siguiente
+                    }
+                    previousHeight = height; //--preparar para el paso siguiente
+                    sectorMinHeight = height;
+                    sectorMaxHeight = height;
+                }
+            }
+        }
+
+        return {
+            upHill: Math.round(uphill),
+            downHill: Math.round(downhill)
+        };
+
+    } else { return null; }
+};
+
 (function () {
     const toolProto = TC.tool.Elevation.prototype;
 
     toolProto.getElevation = function (options) {
         const self = this;
         options = options || {};
+        options.resolution = options.resolution || self.options.resolution;
+        if (options.sampleNumber === undefined) {
+            options.sampleNumber = self.options.sampleNumber;
+        }
         const deferred = $.Deferred();
 
         TC.loadJS(
@@ -61,7 +137,30 @@ TC.tool.Elevation = function (options) {
                         responseType: TC.Consts.mimeType.JSON
                     };
                     if (options.crs) {
-                        data.dataInputs.srid = options.crs;
+                        var idx = options.crs.lastIndexOf(':');
+                        if (idx < 0) {
+                            idx = options.crs.lastIndexOf('#');
+                        }
+                        data.dataInputs.srid = options.crs.substr(idx + 1);
+                    }
+                    if (options.sampleNumber) {
+                        const totalDistance = options.coordinates
+                            .map(function (coord, i, arr) {
+                                const prev = arr[i - 1];
+                                if (prev) {
+                                    const dx = coord[0] - prev[0];
+                                    const dy = coord[1] - prev[1];
+                                    return Math.sqrt(dx * dx + dy * dy);
+                                }
+                                return 0;
+                            })
+                            .reduce(function (prev, curr) {
+                                return prev + curr;
+                            }, 0);
+                        data.dataInputs.splitDistance = totalDistance / options.sampleNumber;
+                    }
+                    else if (options.resolution) {
+                        data.dataInputs.splitDistance = options.resolution;
                     }
                     $.ajax({
                         url: self.url,
@@ -84,10 +183,14 @@ TC.tool.Elevation = function (options) {
 
     toolProto.parseResponse = function (response) {
         var self = this;
-        if (response.coordinates[0][2] < self.minimumElevation) {
-            return null;
+        if (response.coordinates) {
+            response.coordinates.forEach(function (coords) {
+                if (coords[2] < self.minimumElevation) {
+                    coords[2] = null;
+                }
+            });
         }
-        return response.coordinates;
+        return response.coordinates || [];
     };
 
 })();
