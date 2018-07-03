52cfe18e86b4c8283310cc42c2efb8e22f3c8518
flacunzap <flacunza@gmail.com>
2018-07-03 11:43:10 +0200

    Add supported browser versions list

---
+++ TC/config/browser-versions.js
@@ -0,0 +1,30 @@
+﻿TC.Cfg.acceptedBrowserVersions = [
+  {
+    "name": "Chrome",
+    "version": 63
+  },
+  {
+    "name": "Firefox",
+    "version": 57
+  },
+  {
+    "name": "IE",
+    "version": 11
+  },
+  {
+    "name": "Edge",
+    "version": 16
+  },
+  {
+    "name": "Safari",
+    "version": 11
+  },
+  {
+    "name": "Mobile Safari",
+    "version": 11
+  },
+  {
+    "name": "Opera mini",
+    "version": "all"
+  }
+];
