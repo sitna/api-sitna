8301bf1d44ee3ac7ef1db8788c30f720acfba68d
Fernando Lacunza <flacunza@users.noreply.github.com>
2019-05-08 17:00:22 +0200

    Consider basemaps in attributions
    Consider basemaps in attributions.
    Reduce jQuery use.

--- TC/control/Attribution.js
+++ TC/control/Attribution.js
@@ -10,7 +10,7 @@ TC.control.Attribution = function () {
     TC.Control.apply(self, arguments);
 
     self.apiAttribution = '';
-    self.mainDataAttribution = '';
+    self.mainDataAttribution = null;
     self.dataAttributions = [];
     if (self.options.dataAttributions) {
         self.dataAttributions = self.options.dataAttributions instanceof Array ? self.options.dataAttributions : [self.options.dataAttributions];
@@ -28,7 +28,7 @@ TC.inherit(TC.control.Attribution, TC.Control);
         ctlProto.template = TC.apiLocation + "TC/templates/Attribution.html";
     }
     else {
-        ctlProto.template = function () { dust.register(ctlProto.CLASS, body_0); function body_0(chk, ctx) { return chk.w("<div><span>&copy; ").f(ctx.get(["api"], false), ctx, "h", ["s"]).w("</span> - ").h("i18n", ctx, {}, { "$key": "data" }).w(": ").x(ctx.getPath(false, ["mainData", "site"]), ctx, { "else": body_1, "block": body_2 }, {}).x(ctx.get(["otherData"], false), ctx, { "block": body_3 }, {}).w("<div class=\"tc-ctl-attrib-other tc-collapsed\">").s(ctx.get(["otherData"], false), ctx, { "block": body_4 }, {}).w("</div></div>"); } body_0.__dustBody = !0; function body_1(chk, ctx) { return chk.w("<span>&copy; ").f(ctx.getPath(false, ["mainData", "name"]), ctx, "h").w("</span>"); } body_1.__dustBody = !0; function body_2(chk, ctx) { return chk.w("<span>&copy; <a href=\"").f(ctx.getPath(false, ["mainData", "site"]), ctx, "h").w("\" target=\"_blank\">").f(ctx.getPath(false, ["mainData", "name"]), ctx, "h").w("</a></span>"); } body_2.__dustBody = !0; function body_3(chk, ctx) { return chk.w(" - <span class=\"tc-ctl-attrib-cmd\">").h("i18n", ctx, {}, { "$key": "others" }).w("...</span>"); } body_3.__dustBody = !0; function body_4(chk, ctx) { return chk.x(ctx.getPath(false, ["otherData", ctx.get(["$idx"], false), "site"]), ctx, { "else": body_5, "block": body_6 }, {}).h("sep", ctx, { "block": body_7 }, {}); } body_4.__dustBody = !0; function body_5(chk, ctx) { return chk.w("<span>&copy; ").f(ctx.getPath(false, ["otherData", ctx.get(["$idx"], false), "name"]), ctx, "h").w("</span>"); } body_5.__dustBody = !0; function body_6(chk, ctx) { return chk.w("<span>&copy; <a href=\"").f(ctx.getPath(false, ["otherData", ctx.get(["$idx"], false), "site"]), ctx, "h").w("\" target=\"_blank\">").f(ctx.getPath(false, ["otherData", ctx.get(["$idx"], false), "name"]), ctx, "h").w("</a></span>"); } body_6.__dustBody = !0; function body_7(chk, ctx) { return chk.w(", "); } body_7.__dustBody = !0; return body_0 };
+        ctlProto.template = function () { dust.register(ctlProto.CLASS, body_0); function body_0(chk, ctx) { return chk.w("<div><span>&copy; ").f(ctx.get(["api"], false), ctx, "h", ["s"]).w("</span>").x(ctx.get(["mainData"], false), ctx, { "block": body_1 }, {}).x(ctx.get(["otherData"], false), ctx, { "block": body_5 }, {}).w("<div class=\"tc-ctl-attrib-other ").x(ctx.get(["isCollapsed"], false), ctx, { "block": body_6 }, {}).w("\">").s(ctx.get(["otherData"], false), ctx, { "block": body_7 }, {}).w("</div></div>"); } body_0.__dustBody = !0; function body_1(chk, ctx) { return chk.w(" - ").h("i18n", ctx, {}, { "$key": "data" }).w(":").x(ctx.getPath(false, ["mainData", "site"]), ctx, { "else": body_2, "block": body_4 }, {}); } body_1.__dustBody = !0; function body_2(chk, ctx) { return chk.x(ctx.getPath(false, ["mainData", "name"]), ctx, { "block": body_3 }, {}); } body_2.__dustBody = !0; function body_3(chk, ctx) { return chk.w("<span> &copy; ").f(ctx.getPath(false, ["mainData", "name"]), ctx, "h").w("</span>"); } body_3.__dustBody = !0; function body_4(chk, ctx) { return chk.w("<span> &copy; <a href=\"").f(ctx.getPath(false, ["mainData", "site"]), ctx, "h").w("\" target=\"_blank\">").f(ctx.getPath(false, ["mainData", "name"]), ctx, "h").w("</a></span>"); } body_4.__dustBody = !0; function body_5(chk, ctx) { return chk.w(" - <span class=\"tc-ctl-attrib-cmd\">").h("i18n", ctx, {}, { "$key": "others" }).w("...</span>"); } body_5.__dustBody = !0; function body_6(chk, ctx) { return chk.w(" tc-collapsed "); } body_6.__dustBody = !0; function body_7(chk, ctx) { return chk.x(ctx.getPath(false, ["otherData", ctx.get(["$idx"], false), "site"]), ctx, { "else": body_8, "block": body_9 }, {}).h("sep", ctx, { "block": body_10 }, {}); } body_7.__dustBody = !0; function body_8(chk, ctx) { return chk.w("<span>&copy; ").f(ctx.getPath(false, ["otherData", ctx.get(["$idx"], false), "name"]), ctx, "h").w("</span>"); } body_8.__dustBody = !0; function body_9(chk, ctx) { return chk.w("<span>&copy; <a href=\"").f(ctx.getPath(false, ["otherData", ctx.get(["$idx"], false), "site"]), ctx, "h").w("\" target=\"_blank\">").f(ctx.getPath(false, ["otherData", ctx.get(["$idx"], false), "name"]), ctx, "h").w("</a></span>"); } body_9.__dustBody = !0; function body_10(chk, ctx) { return chk.w(", "); } body_10.__dustBody = !0; return body_0 };
     }
 
     ctlProto.register = function (map) {
@@ -42,7 +42,7 @@ TC.inherit(TC.control.Attribution, TC.Control);
                 // TODO: sanitizer
                 var attr = obj.getAttribution();
                 if (attr) {
-                    if (/IDENA/.test(attr.name)) {
+                    if (/IDENA/.test(attr.name) || /Tracasa Instrumental/.test(attr.name)) {
                         self.mainDataAttribution = {
                             name: 'IDENA',
                             site: 'http://idena.navarra.es/'
@@ -79,7 +79,7 @@ TC.inherit(TC.control.Attribution, TC.Control);
                     }
 
                     return true;
-                };                
+                };
 
                 if (obj instanceof TC.Layer ? checkRemoveData() : true) {
                     // TODO: sanitizer
@@ -92,8 +92,32 @@ TC.inherit(TC.control.Attribution, TC.Control);
                             }
                             return prev;
                         }, -1);
+
+                        const checkIsSameAttribution = function (toCheckName) {
+                            return (/IDENA/.test(attr.name) || /Tracasa Instrumental/.test(attr.name)) &&
+                                (/IDENA/.test(toCheckName) || /Tracasa Instrumental/.test(toCheckName)) ||
+                                    (attr.name === toCheckName);
+                        };
+
+                        // Validamos si las atribuciones a borrar son también del mapa base
+                        if (self.map.baseLayer && self.map.baseLayer.wrap.getAttribution() && checkIsSameAttribution(self.map.baseLayer.wrap.getAttribution().name)) {
+                            return;
+                        } else {
+                            // Validamos si las atribuciones a borrar son también de alguna de las capas raster cargadas
+                            if (self.map.workLayers.filter(function (layer) {
+                                return layer.type === TC.Consts.layerType.WMS || layer.type === TC.Consts.layerType.WMTS;
+                            }).some(function (layer) {
+                                var workLayerAttribution = layer.wrap.getAttribution();
+                                return workLayerAttribution && checkIsSameAttribution(workLayerAttribution.name);
+                            })) {
+                                return;
+                            }
+                        }
+
                         if (index > -1) {
                             self.dataAttributions.splice(index, 1);
+                        } else if (/IDENA/.test(attr.name) || /Tracasa Instrumental/.test(attr.name)) {
+                            self.mainDataAttribution = null;
                         }
                     }
                 }
@@ -102,40 +126,75 @@ TC.inherit(TC.control.Attribution, TC.Control);
 
         self.render();
 
-        map.on(TC.Consts.event.LAYERADD, function (e) {
-            if (e.layer.wrap.getAttribution) {
-                addData(e.layer.wrap);
+        map.loaded(function () {
+            if (map.baseLayer.wrap.getAttribution) {
+                addData(map.baseLayer.wrap);
                 self.render();
             }
-        });        
+        });
+
+        map.on(TC.Consts.event.LAYERADD, function (e) {
+            const layer = e.layer;
+            if (!layer.isBase && layer.wrap.getAttribution) {
+                addData(layer.wrap);
+                self.render();
+            }
+        });
+
+        map.on(TC.Consts.event.BEFOREBASELAYERCHANGE + " " + TC.Consts.event.OVERVIEWBASELAYERCHANGE, function (e) {
+            const type = e.type;
+            const newLayer = e.newLayer;
+            const oldLayer = e.oldLayer;
+            if (TC.Consts.event.OVERVIEWBASELAYERCHANGE.indexOf(type) > -1) {
+                self.ignoreLayer = newLayer;
+            }
+
+            if (oldLayer && oldLayer.wrap.getAttribution) {
+                removeData(oldLayer.wrap);
+            }
+
+            if (newLayer && newLayer.wrap.getAttribution) {
+                addData(newLayer.wrap);
+            }
+
+            self.render();
+        });
 
         map.on(TC.Consts.event.LAYERREMOVE, function (e) {
-            if (e.layer.wrap.getAttribution) {
-                removeData(e.layer.wrap);
+            const layer = e.layer;
+            if (layer.wrap.getAttribution) {
+                removeData(layer.wrap);
                 self.render();
             }
         });
 
         map.on(TC.Consts.event.TERRAINPROVIDERADD, function (e) {
-            if (e.terrainProvider.getAttribution) {
-                addData(e.terrainProvider);
+            const terrainProvider = e.terrainProvider;
+            if (terrainProvider.getAttribution) {
+                addData(terrainProvider);
                 self.render();
             }
         });
 
         map.on(TC.Consts.event.TERRAINPROVIDERREMOVE, function (e) {
-            if (e.terrainProvider.getAttribution) {
-                removeData(e.terrainProvider);
+            const terrainProvider = e.terrainProvider;
+            if (terrainProvider.getAttribution) {
+                removeData(terrainProvider);
                 self.render();
             }
         });
 
         map.on(TC.Consts.event.LAYERVISIBILITY, function (e) {
-            if (e.layer.wrap.getAttribution) {
-                if (e.layer.getVisibility()) {
-                    addData(e.layer.wrap);
+            const layer = e.layer;
+            if (self.ignoreLayer === layer) {
+                return;
+            }
+
+            if (layer.wrap.getAttribution) {
+                if (layer.getVisibility()) {
+                    addData(layer.wrap);
                 } else {
-                    removeData(e.layer.wrap);
+                    removeData(layer.wrap);
                 }
                 self.render();
             }
@@ -145,16 +204,33 @@ TC.inherit(TC.control.Attribution, TC.Control);
     };
 
     ctlProto.render = function (callback) {
-        var self = this;
-        self.renderData({ api: self.apiAttribution, mainData: self.mainDataAttribution, otherData: self.dataAttributions }, function () {
-            self._$div.find('.' + self.CLASS + '-cmd').on(TC.Consts.event.CLICK, function () {
-                self._$div.find('.' + self.CLASS + '-other').toggleClass(TC.Consts.classes.COLLAPSED);
+        const self = this;
+
+        return self._set1stRenderPromise(self.renderData({
+            api: self.apiAttribution,
+            mainData: self.mainDataAttribution,
+            otherData: self.dataAttributions,
+            isCollapsed: self.div.querySelector('.' + self.CLASS + '-other') ? self.div.querySelector('.' + self.CLASS + '-other').classList.contains(TC.Consts.classes.COLLAPSED) : true
+        }, function () {
+            const cmd = self.div.querySelector('.' + self.CLASS + '-cmd');
+            cmd && cmd.addEventListener(TC.Consts.event.CLICK, function () {
+                self.toggleOtherAttributions();
             });
 
-            if ($.isFunction(callback)) {
+            if (typeof callback === 'function') {
                 callback();
             }
-        });
+        }));
     };
 
+    ctlProto.toggleOtherAttributions = function () {
+        const self = this;
+        const other = self.div.querySelector('.' + self.CLASS + '-other');
+        if (other.classList.contains(TC.Consts.classes.COLLAPSED)) {
+            other.classList.remove(TC.Consts.classes.COLLAPSED);
+        }
+        else {
+            other.classList.add(TC.Consts.classes.COLLAPSED);
+        }
+    };
 })();
