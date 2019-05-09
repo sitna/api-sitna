5e27106d8662810a88dd0218ffee7eabdf9f8e72
Fernando Lacunza <flacunza@users.noreply.github.com>
2019-05-09 09:00:05 +0200

    Create NavBarHome.js

---
+++ TC/Control/NavBarHome.js
@@ -0,0 +1,41 @@
+﻿TC.control = TC.control || {};
+
+if (!TC.Control) {
+    TC.syncLoadJS(TC.apiLocation + 'TC/Control');
+}
+
+TC.control.NavBarHome = function () {
+    TC.Control.apply(this, arguments);
+};
+
+TC.inherit(TC.control.NavBarHome, TC.Control);
+
+(function () {
+    var ctlProto = TC.control.NavBarHome.prototype;
+
+    ctlProto.CLASS = 'tc-ctl-nav-home';
+
+    ctlProto.render = function () {
+        var self = this;
+        if (!self.wrap) {
+            self.wrap = new TC.wrap.control.NavBarHome(self);
+        }
+        return Promise.resolve();
+    };
+
+    ctlProto.register = function (map) {
+        const self = this;
+        const result = TC.Control.prototype.register.call(self, map);
+        self.wrap.register(map);        
+
+        map.on(TC.Consts.event.PROJECTIONCHANGE, function (e) {
+            const crs = e.crs;
+            var bottomLeft = TC.Util.reproject([map.options.initialExtent[0], map.options.initialExtent[1]], map.options.crs, crs);
+            var topRight = TC.Util.reproject([map.options.initialExtent[2], map.options.initialExtent[3]], map.options.crs, crs);
+            self.wrap.setInitialExtent([bottomLeft[0], bottomLeft[1], topRight[0], topRight[1]]);
+        });
+
+        return result;
+    };
+
+})();
