06a1f5e279bbe9a832d2b2818bcd3329f54dec18
Fernando Lacunza <flacunza@users.noreply.github.com>
2019-10-03 11:39:13 +0200

    Add setCoords method, remove jQuery

--- TC/feature/MultiPolyline.js
+++ TC/feature/MultiPolyline.js
@@ -9,18 +9,13 @@ if (!TC.Feature) {
  * Parameters: coords, array of 2 element arrays of numbers; options, object
  */
 TC.feature.MultiPolyline = function (coords, options) {
-    var self = this;
+    const self = this;
 
     TC.Feature.apply(self, arguments);
 
-    var opts;
-    if (self.wrap.isNative(coords)) {
-        coords._wrap = self.wrap;
-        self.wrap.feature = coords;
-    }
-    else {
-        opts = self.options = $.extend(true, self.options, TC.Cfg.styles.line, options);
-        self.wrap.createMultiPolyline(coords, opts);
+    if (!self.wrap.isNative(coords)) {
+        options = self.options = TC.Util.extend(true, self.options, TC.Cfg.styles.line, options);
+        self.wrap.createMultiPolyline(coords, options);
     }
 };
 
@@ -37,6 +32,14 @@ TC.feature.MultiPolyline.prototype.getCoords = function (options) {
         return [].concat.apply([], coords);
     }
     return coords;
+};
+
+TC.feature.MultiPolyline.prototype.setCoords = function (coords) {
+    const self = this;
+    if (Array.isArray(coords) && Array.isArray(coords[0]) && !Array.isArray(coords[0][0])) {
+        coords = [coords];
+    }
+    return TC.Feature.prototype.setCoords.call(self, coords);
 };
 
 TC.feature.MultiPolyline.prototype.getLength = function (options) {
