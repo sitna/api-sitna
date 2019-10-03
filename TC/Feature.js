442687f486b659f1576cb9cf240d1de94387896a
Fernando Lacunza <flacunza@users.noreply.github.com>
2019-10-03 11:42:32 +0200

    Update Feature.js
    Remove jQuery use.
    Constructor sets _wrap property.
    Add numeric value validation to setCoords.
    Add link titles to getInfo output.

--- TC/Feature.js
+++ TC/Feature.js
@@ -4,19 +4,18 @@ TC.Feature = function (coords, options) {
 
     self.wrap = new TC.wrap.Feature();
     self.wrap.parent = self;
-
-    if ($.isArray(coords)) {
-        self.geometry = coords;
-    }
-    else if (self.wrap.isNative(coords)) {
+    if (self.wrap.isNative(coords)) {
         self.wrap.feature = coords;
+        coords._wrap = self.wrap;
         self.id = self.wrap.getId();
         self.geometry = self.wrap.getGeometry();
-        self.folders = coords._folders;
+        if (coords._folders) {
+            self.folders = coords._folders;
+        }
         self.data = self.wrap.getData();
     }
 
-    var opts = self.options = $.extend(true, {}, options);
+    var opts = self.options = TC.Util.extend(true, {}, options);
 
     self.id = self.id || opts.id || TC.getUID();
     self.data = opts.data || self.data || null;
@@ -106,7 +105,7 @@ TC.Feature.prototype.getCoords = function () {
 TC.Feature.prototype.getCoordsArray = function () {
     const self = this;
     const isPoint = function (elm) {
-        return $.isArray(elm) && elm.length >= 2 && typeof elm[0] === 'number' && typeof elm[1] === 'number';
+        return Array.isArray(elm) && elm.length >= 2 && typeof elm[0] === 'number' && typeof elm[1] === 'number';
     };
     const flattenFn = function (val) {
         return isPoint(val) ? [val] : val.reduce(reduceFn, []);
@@ -120,7 +119,7 @@ TC.Feature.prototype.getCoordsArray = function () {
         }
         return acc;
     };
-    return flattenFn(this.getCoords());
+    return flattenFn(self.getCoords());
 };
 
 TC.Feature.prototype.getGeometryStride = function () {
@@ -136,6 +135,25 @@ TC.Feature.prototype.getGeometryStride = function () {
 
 TC.Feature.prototype.setCoords = function (coords) {
     const self = this;
+
+    const toNumberCoords = function (arr) {
+        arr.forEach(function (elm, idx) {
+            if (Array.isArray(elm)) {
+                toNumberCoords(elm);
+            }
+            else {
+                if (typeof elm !== 'number') {
+                    console.log('Warning: coordinate does not have number type');
+                    arr[idx] = parseFloat(elm);
+                }
+            }
+        });
+    };
+
+    if (Array.isArray(coords)) {
+        toNumberCoords(coords);
+    }
+
     self.geometry = coords;
     return self.wrap.setGeometry(coords);
 };
@@ -154,7 +172,7 @@ TC.Feature.prototype.getData = function () {
 
 TC.Feature.prototype.setData = function (data) {
     var self = this;
-    self.data = $.extend(self.data, data);
+    self.data = TC.Util.extend(self.data, data);
     self.wrap.setData(data);
 };
 
@@ -182,7 +200,8 @@ TC.Feature.prototype.getInfo = function (options) {
         else {
             var html = [];
             const hSlots = [];
-            var openText = TC.Util.getLocaleString(locale, 'open');
+            const openText = TC.Util.getLocaleString(locale, 'open');
+            const titleText = TC.Util.getLocaleString(locale, 'linkInNewWindow');
             for (var key in data) {
                 const value = data[key];
                 const match = key.match(/^h(\d)_/i);
@@ -198,7 +217,9 @@ TC.Feature.prototype.getInfo = function (options) {
                         if (isUrl) {
                             html[html.length] = '<a href="';
                             html[html.length] = value;
-                            html[html.length] = '" target="_blank">';
+                            html[html.length] = '" target="_blank" title="';
+                            html[html.length] = titleText;
+                            html[html.length] = '">';
                             html[html.length] = openText;
                             html[html.length] = '</a>';
                         }
@@ -378,17 +399,17 @@ TC.Feature.prototype.select = function () {
         self.layer.selectedFeatures.push(self);
     }
     var selectionOptions = self.options.selection || {};
-    self.setStyle($.extend({}, TC.Cfg.styles.selection[self.STYLETYPE], selectionOptions[self.STYLETYPE]));
+    self.setStyle(TC.Util.extend({}, TC.Cfg.styles.selection[self.STYLETYPE], selectionOptions[self.STYLETYPE]));
 };
 
 TC.Feature.prototype.unselect = function () {
-    var self = this;
+    const self = this;
     self._selected = false;
     // Volvemos al estilo por defecto
     self.setStyle(self.options);
 
     if (self.layer) {
-        var idx = $.inArray(self, self.layer.selectedFeatures);
+        const idx = self.layer.selectedFeatures.indexOf(self);
         if (idx >= 0) {
             self.layer.selectedFeatures.splice(idx, 1);
         }
