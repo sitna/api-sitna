6308d92810e96848119c82bc57b6b187593fdb9a
Fernando Lacunza <flacunza@users.noreply.github.com>
2022-12-09 14:13:59 +0100

    Add webpack configuration

---
+++ webpack/precompiledTemplates.js
@@ -0,0 +1,59 @@
+﻿const handlebars = require('handlebars');
+const fs = require('fs');
+const path = require('path');
+
+(function (root, factory) {
+    if (typeof exports === "object") { // CommonJS
+        module.exports = factory();
+    } else if (typeof define === "function" && define.amd) { // AMD
+        define([], factory);
+    } else {
+        root.precompiledTemplates = factory();
+    }
+})(this, function () {
+    const precompiledTemplates = {};
+    const ourPath = path.resolve(__dirname, '../TC/templates/');
+    fs.readdir(ourPath, function (err, files) {
+        if (err) {
+            return console.error(err);
+        }
+        files
+            .filter(f => f.endsWith('.hbs'))
+            .forEach(function (file) {
+                precompiledTemplates[file] = handlebars
+                    .precompile(fs.readFileSync(ourPath + '\\' + file, "utf8"), {
+                        knownHelpers: {
+                            i18n: true,
+                            gt: true,
+                            lt: true,
+                            eq: true,
+                            every: true,
+                            round: true,
+                            lowerCase: true,
+                            startsWith: true,
+                            numberSeparator: true,
+                            countif: true,
+                            isEmpty: true,
+                            getId: true,
+                            isArray: true,
+                            isObject: true,
+                            formatNumber: true,
+                            isUrl: true,
+                            getYoutubeId: true,
+                            isVideoAttribute: true,
+                            isAudioAttribute: true,
+                            isImageAttribute: true,
+                            isEmbedAttribute: true,
+                            getTagWidth: true,
+                            getTagHeight: true,
+                            getHeader: true,
+                            removeSpecialAttributeTag: true,
+                            formatDateOrNumber: true
+                        },
+                        knownHelpersOnly: true
+                    })
+                    .replace(/\n/g, "");
+            });
+    });
+    return precompiledTemplates;
+});
---
+++ webpack/webpack.config.debug.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const webpack = require('webpack');
+module.exports = {
+    entry: './sitna.js',
+    mode: 'development',
+    devtool: 'source-map',
+    resolve: {
+        fallback: {
+            buffer: require.resolve('buffer/')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                enforce: 'pre',
+                use: ['source-map-loader']
+            },
+            {
+                test: path.resolve(__dirname, '../sitna.js'),
+                loader: 'string-replace-loader',
+                options: {
+                    // Añadimos la fecha de compilación a la cadena indicadora de versión
+                     search: /TC\.version = '(\d+\.\d+\.\d+)';/,
+                    replace(match, p1) {
+                        return "TC.version = '" + p1 + " [" + (new Date()).toLocaleString() + "]';";
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            Buffer: ['buffer', 'Buffer'],
+        }),
+    ],
+    devServer: {
+        allowedHosts: 'auto',
+        static: './dist',
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+        }
+    },
+    output: {
+        filename: 'sitna.debug.js',
+        path: path.resolve(__dirname, '../dist'),
+        chunkFilename: 'chunks/[name].sitna.debug.js',
+        sourceMapFilename: 'maps/[file].map',
+        library: 'SITNA',
+        libraryTarget: 'umd'
+    }
+};
