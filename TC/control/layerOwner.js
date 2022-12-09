577ff016cd471c5a22455ffab75a0623ec83656e
Fernando Lacunza <flacunza@users.noreply.github.com>
2022-12-09 13:39:34 +0100

    Create layerOwner mixin

---
+++ TC/control/layerOwner.js
@@ -0,0 +1,39 @@
+﻿import TC from '../../TC';
+import Consts from '../Consts';
+
+
+// Mixin
+const layerOwner = {
+
+    getLayers: function () {
+        const self = this;
+        if (!self.layers) {
+            self.layers = [];
+        }
+        return self.layers;
+    },
+
+    registerLayerAdd: function () {
+        const self = this;
+        self.map.on(Consts.event.LAYERADD, function (e) {
+            const match = self.getIdCount(e.layer.id);
+            if (match) {
+                TC.setUIDStart(match + 1, { prefix: self.id + '-' });
+                e.layer.owner = self;
+            }
+        });
+    },
+
+    getIdCount: function (id) {
+        const self = this;
+        const regEx = new RegExp(`^${self.id}(-(\\d+))*$`);
+        const match = id.match(regEx);
+        if (match) {
+            return parseInt(match[1]);
+        }
+        return 0;
+    }
+
+};
+
+export default layerOwner;
