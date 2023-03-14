ec7953f886711201100cfa2767d073cc4f0c8b72
Fernando Lacunza <flacunza@users.noreply.github.com>
2023-03-14 09:23:34 +0100

    Refactor

--- TC/i18n.js
+++ TC/i18n.js
@@ -1,32 +1,21 @@
 ﻿import TC from '../TC';
 import Consts from './Consts';
 const i18n = {};
-i18n.loadResources = function (condition, path, locale) {
-    var result;
+i18n.loadResources = async function (condition, path, locale) {
     if (condition) {
-        result = new Promise(function (resolve, reject) {
-            TC.ajax({
-                url: path + locale + '.json',
-                method: 'GET',
-                responseType: Consts.mimeType.JSON
-            })
-                .then(function (response) {
-                    const data = response.data;
-                    i18n[locale] = i18n[locale] || {};
-                    Object.assign(i18n[locale], data);
-                    i18n.currentLocale = i18n[locale];
-                    i18n.currentLocaleKey = locale;
-                    resolve();
-                })
-                .catch(function (err) {
-                    reject(err);
-                });
-        });
+        const response = await TC.ajax({
+            url: path + locale + '.json',
+            method: 'GET',
+            responseType: Consts.mimeType.JSON
+        })
+        const data = response.data;
+        i18n[locale] = i18n[locale] || {};
+        Object.assign(i18n[locale], data);
+        i18n.currentLocale = i18n[locale];
+        i18n.currentLocaleKey = locale;
     } else {
         i18n.currentLocale = i18n[locale];
-        result = Promise.resolve();
     }
-    return result;
 };
 
 export default i18n;
