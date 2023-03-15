5b339ef953e5760bc447341ca3d3a0c5761b65d8
Fernando Lacunza <flacunza@users.noreply.github.com>
2023-03-15 16:20:47 +0100

    Rename classes, ids, use highlight methods

--- TC/layout/responsive/script.js
+++ TC/layout/responsive/script.js
@@ -43,26 +43,29 @@ document.querySelectorAll('.tc-map').forEach(function (elm) {
 
         // En pantalla estrecha cambiamos el método de mostrar GFI.
         if (window.matchMedia('screen and (max-width: 40em), screen and (max-height: 40em)').matches) {
-            map.defaultInfoContainer = TC.Consts.infoContainer.RESULTS_PANEL;
+            map.defaultInfoContainer = SITNA.Consts.infoContainer.RESULTS_PANEL;
         }
 
         map.ready(function () {
+
             // Colapsamos los controles de tipo web component, porque no los podemos colapsar por markup
             map.controls.forEach(c => {
                 if (c instanceof HTMLElement) {
-                    if (c.parentElement && c.parentElement.classList.contains(TC.Consts.classes.COLLAPSED)) {
-                        c.classList.add(TC.Consts.classes.COLLAPSED);
+                    if (c.parentElement && c.parentElement.classList.contains(SITNA.Consts.classes.COLLAPSED)) {
+                        c.classList.add(SITNA.Consts.classes.COLLAPSED);
                     }
                 }
             });
 
-            const ovPanel = map.div.querySelector('.ovmap-panel');
-            const rcollapsedClass = 'right-collapsed';
-            const lcollapsedClass = 'left-collapsed';
-            var ovmap;
+            /* --- LEGACY --- */
+            const ovPanel = map.div.querySelector(`.${TC.Consts.classes.OVERVIEW_MAP_PANEL},.ovmap-panel`);
+            const rcollapsedClass = TC.Consts.classes.COLLAPSED_RIGHT || 'right-collapsed';
+            const lcollapsedClass = TC.Consts.classes.COLLAPSED_LEFT || 'left-collapsed';
 
-            map.div.querySelectorAll('.right-panel > h1').forEach(function (h1) {
-                h1.addEventListener(TC.Consts.event.CLICK, function (e) {
+            const ovmap = map.getControlsByClass('TC.control.OverviewMap')[0];
+
+            map.div.querySelectorAll(/* --- LEGACY --- */`.${TC.Consts.classes.RIGHT_PANEL} > h1, .right-panel > h1`).forEach(function (elm) {
+                elm.addEventListener(SITNA.Consts.event.CLICK, function (e) {
                     e.preventDefault();
                     e.stopPropagation();
                     const tab = e.target;
@@ -83,8 +86,8 @@ document.querySelectorAll('.tc-map').forEach(function (elm) {
                 });
             });
 
-            map.div.querySelectorAll('.left-panel > h1').forEach(function (h1) {
-                h1.addEventListener(TC.Consts.event.CLICK, function (e) {
+            map.div.querySelectorAll(`.${TC.Consts.classes.LEFT_PANEL} > h1`).forEach(function (h1) {
+                h1.addEventListener(SITNA.Consts.event.CLICK, function (e) {
                     e.preventDefault();
                     e.stopPropagation();
                     const panel = e.target.parentElement;
@@ -92,29 +95,78 @@ document.querySelectorAll('.tc-map').forEach(function (elm) {
                 });
             });
 
-            map.div.querySelector('.tools-panel').addEventListener(TC.Consts.event.CLICK, function (e) {
-                let tab = e.target;
-                if (tab.tagName === "BUTTON" && tab.closest("div.tc-ctl." + TC.Consts.classes.COLLAPSED))tab=tab.parentElement;
-                if (tab.tagName === ('H2')) {
-                    const ctlDiv = tab.parentElement;
-                    if (map && map.layout && map.layout.accordion) {
-                        if (ctlDiv.classList.contains(TC.Consts.classes.COLLAPSED)) {
-                            this.querySelectorAll('h2').forEach(function (h2) {
-                                const div = h2.parentElement;
-                                if (div !== ctlDiv && !div.matches('.tc-ctl-search')) {
-                                    div.classList.add(TC.Consts.classes.COLLAPSED);
-                                }
-                            });
+            /* --- LEGACY --- */
+            const toolsPanel = map.div.querySelector('.' + TC.Consts.classes.TOOLS_PANEL) || map.div.querySelector('#tools-panel');
+
+            const collapseExceptions = map
+                .getControlsByClass(TC.control.Search)
+                .concat(map.getControlsByClass(TC.control.LanguageSelector))
+                .concat(map.controls.filter(c => c.containerControl))
+                .map(c => c.id)
+                .filter(id => toolsPanel.querySelector('#' + id));
+
+            const toggleCollapsed = function (value) {
+                const self = this;
+                let element;
+                if (self instanceof TC.control.WebComponentControl) {
+                    element = self;
+                }
+                else {
+                    element = self.div;
+                }
+                if (!collapseExceptions.includes(self.id)) {
+                    element.classList.toggle(SITNA.Consts.classes.COLLAPSED, value);
+                }
+            };
+
+            map
+                .on(SITNA.Consts.event.CONTROLHIGHLIGHT, function (e) {
+                    const ctl = e.control;
+                    toggleCollapsed.call(ctl, false);
+
+                    if (map.layout && map.layout.accordion) {
+                        // Hacemos que solamente un control del panel de herramientas esté desplegado cada vez
+                        const toolPanelControls = map
+                            .controls
+                            .filter(ctl => !ctl.containerControl)
+                            .filter(ctl => ctl.div && toolsPanel.contains(ctl.div));
+                        if (toolPanelControls.includes(ctl)) {
+                            toolPanelControls
+                                .filter(c => c !== ctl)
+                                .forEach(ctl => ctl.unhighlight());
                         }
                     }
-                    ctlDiv.classList.toggle(TC.Consts.classes.COLLAPSED);
+                })
+                .on(SITNA.Consts.event.CONTROLUNHIGHLIGHT, function (e) {
+                    const ctl = e.control;
+                    toggleCollapsed.call(ctl, true);
+                });
+
+            toolsPanel.addEventListener(SITNA.Consts.event.CLICK, function (e) {
+                let tab = e.target;
+                if (tab.tagName === "BUTTON" && tab.closest("div.tc-ctl." + SITNA.Consts.classes.COLLAPSED)) {
+                    tab = tab.parentElement;
+                }
+                if (tab.tagName === ('H2')) {
+                    for (var i = 0; i < map.controls.length; i++) {
+                        const ctl = map.controls[i];
+                        if (ctl.div && ctl.div.contains(tab)) {
+                            if (ctl.isHighlighted()) {
+                                ctl.unhighlight();
+                            }
+                            else {
+                                ctl.highlight();
+                            }
+                            break;
+                        }
+                    }
                 }
             });          
 
 
             map.loaded(function () {
 
-                ovmap = map.getControlsByClass('TC.control.OverviewMap')[0];
+                const ovmap = map.getControlsByClass('TC.control.OverviewMap')[0];
                 if (ovmap) {
                     ovmap.loaded(function () {
                         ovmap.disable();
@@ -137,13 +189,14 @@ document.querySelectorAll('.tc-map').forEach(function (elm) {
                             "event": "click",
                             "elements": [
                                 ".tc-ctl-bms-node > label",
-                                ".tc-ctl-meas-select label",
-                                ".tc-ctl-mod-btn-select"
+                                ".tc-ctl-meas-select sitna-tab",
+                                ".tc-ctl-mod-btn-select",
+                                ".tc-ctl-mod-btn-del-vertex"
                             ],
                             "changes": [
                                 {
-                                    "targets": "#tools-panel",
-                                    "classes": "right-collapsed"
+                                    "targets": '.' + TC.Consts.classes.TOOLS_PANEL,
+                                    "classes": rcollapsedClass
                                 }
                             ]
                         }
@@ -151,21 +204,21 @@ document.querySelectorAll('.tc-map').forEach(function (elm) {
                 ]);
             });
 
-            TC.Consts.event.TOOLSCLOSE = TC.Consts.event.TOOLSCLOSE || 'toolsclose.tc';
-            TC.Consts.event.TOOLSOPEN = TC.Consts.event.TOOLSOPEN || 'toolsopen.tc';
+            SITNA.Consts.event.TOOLSCLOSE = SITNA.Consts.event.TOOLSCLOSE || 'toolsclose.tc';
+            SITNA.Consts.event.TOOLSOPEN = SITNA.Consts.event.TOOLSOPEN || 'toolsopen.tc';
 
-            map.on(TC.Consts.event.TOOLSOPEN, function (e) {
-                map.div.querySelector('.tools-panel').classList.remove(rcollapsedClass);
+            map.on(SITNA.Consts.event.TOOLSOPEN, function (_e) {
+                map.div.querySelector('.' + TC.Consts.classes.TOOLS_PANEL).classList.remove(rcollapsedClass);
             });
 
-            map.on(TC.Consts.event.TOOLSCLOSE, function (e) {
-                map.div.querySelector('.tools-panel').classList.add(rcollapsedClass);
+            map.on(SITNA.Consts.event.TOOLSCLOSE, function (_e) {
+                map.div.querySelector('.' + TC.Consts.classes.TOOLS_PANEL).classList.add(rcollapsedClass);
             });
 
             if (TC.browserFeatures.touch()) {
                 const addSwipe = function (direction) {
                     const selector = '.' + direction + '-panel';
-                    const className = direction + '-collapsed';
+                    const className = 'tc-collapsed-' + direction;
                     const options = { noSwipe: 'li,a' };
                     options[direction] = function () {
                         this.classList.add(className);
