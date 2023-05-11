03ce430f0b1168fc1b91d224b1cf6521174f74cf
Fernando Lacunza <flacunza@users.noreply.github.com>
2023-05-11 10:46:58 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import { Cfg, Map, Consts, feature, layer } from './sitna.js';
+globalThis.SITNA = {
+    Cfg: Cfg,
+    Map: Map,
+    Consts: Consts,
+    feature: feature,
+    layer: layer
+};
+export { Map as default, Cfg, Consts, feature, layer };
