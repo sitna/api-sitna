6a744a8e167c0ef70102b8d87bff19b38d51048b
Fernando Lacunza <flacunza@users.noreply.github.com>
2023-05-11 10:39:27 +0200

    Remove race condition in rendering

--- TC/control/Share.js
+++ TC/control/Share.js
@@ -19,10 +19,6 @@ class Share extends MapInfo {
         const self = this;
         self.div.classList.add(self.CLASS);
 
-        self.template = {};
-        self.template[self.CLASS] = TC.apiLocation + "TC/templates/tc-ctl-share.hbs";
-        self.template[self.CLASS + '-dialog'] = TC.apiLocation + "TC/templates/tc-ctl-share-dialog.hbs";
-
         self.#dialogDiv = Util.getDiv(self.options.dialogDiv);
         if (!self.options.dialogDiv) {
             document.body.appendChild(self.#dialogDiv);
@@ -68,158 +64,171 @@ class Share extends MapInfo {
         return self;
     }
 
+    async loadTemplates() {
+        const self = this;
+        const mainTemplatePromise = import('../templates/tc-ctl-share.mjs');
+        const dialogTemplatePromise = import('../templates/tc-ctl-share-dialog.mjs');
+
+        const template = {};
+        template[self.CLASS] = (await mainTemplatePromise).default;
+        template[self.CLASS + '-dialog'] = (await dialogTemplatePromise).default;
+        self.template = template;
+    }
+
     async render(callback) {
         const self = this;
         self.#dialogDiv.innerHTML = await self.getRenderedHtml(self.CLASS + '-dialog', null);
-        return await self._set1stRenderPromise(super.renderData.call(self, { controlId: self.id }, function () {
-            //Si el navegador no soporta copiar al portapapeles, ocultamos el botón de copiar
-            if (document.queryCommandSupported && document.queryCommandSupported('copy')) {
-                self.div.querySelectorAll('button').forEach(function (btn) {
-                    btn.classList.remove('hide');
-                });
-                self.div.querySelectorAll('input[type=text]').forEach(function (input) {
-                    delete input.dataset.dataOriginalTitle;
-                });
-            }
+        const renderPromise = super.renderData.call(self, { controlId: self.id });
+        self._set1stRenderPromise(renderPromise);
+        await renderPromise;
 
-            self.div.querySelector('h2').addEventListener('click', function (_e) {
-                self.update();
+        //Si el navegador no soporta copiar al portapapeles, ocultamos el botón de copiar
+        if (document.queryCommandSupported && document.queryCommandSupported('copy')) {
+            self.div.querySelectorAll('button').forEach(function (btn) {
+                btn.classList.remove('hide');
             });
+            self.div.querySelectorAll('input[type=text]').forEach(function (input) {
+                delete input.dataset.dataOriginalTitle;
+            });
+        }
 
-            // Si el SO no es móvil, ocultamos el botón de compartir a WhatsApp
-            if (!Util.detectMobile()) {
-                self.div.querySelector(`sitna-button.${self.CLASS}-btn-whatsapp`).classList.add(Consts.classes.HIDDEN);
-            }
+        self.div.querySelector('h2').addEventListener('click', function (_e) {
+            self.update();
+        });
 
-            self.div.querySelector(`.${self.CLASS}-url-box button.${self.CLASS}-btn-shorten`).addEventListener('click', function (e) {
+        // Si el SO no es móvil, ocultamos el botón de compartir a WhatsApp
+        if (!Util.detectMobile()) {
+            self.div.querySelector(`sitna-button.${self.CLASS}-btn-whatsapp`).classList.add(Consts.classes.HIDDEN);
+        }
+
+        self.div.querySelector(`.${self.CLASS}-url-box button.${self.CLASS}-btn-shorten`).addEventListener('click', function (e) {
+            const btn = e.target;
+            self.#selectInputField(btn, true);
+        });
+
+        self.div.querySelectorAll(`.${self.CLASS}-url-box button.${self.CLASS}-btn-copy`).forEach(elm => {
+            elm.addEventListener('click', async function (e) {
                 const btn = e.target;
-                self.#selectInputField(btn, true);
+
+                await self.#selectInputField(btn);
+
+                document.execCommand('copy');
+
+                btn.textContent = self.getLocaleString('copied');
+
+                setTimeout(function () {
+                    btn.textContent = self.getLocaleString('copy');
+                    self.#unselectInputField();
+                }, 1000);
             });
+        });
 
-            self.div.querySelectorAll(`.${self.CLASS}-url-box button.${self.CLASS}-btn-copy`).forEach(elm => {
-                elm.addEventListener('click', async function (e) {
-                    const btn = e.target;
+        self.div.querySelector('input[type=text]').addEventListener('click', function (e) {
+            self.#selectInputField(e.target);
+        });
 
-                    await self.#selectInputField(btn);
+        //Enviar por e-mail
+        self.div.querySelector(`sitna-button.${self.CLASS}-btn-email`).addEventListener('click', async function (_e) {
+            const url = await self.shortenedLink();
+            if (url) {
+                const body = encodeURIComponent(url + "\n");
+                if (body.length > self.MAILTO_MAX_LENGTH) {
+                    self.map.toast(self.getLocaleString('urlTooLongForMailto'), { type: Consts.msgType.WARNING });
+                }
+                window.location.href = 'mailto:?body=' + body;
+            }
+        });
 
-                    document.execCommand('copy');
+        //Generar código QR        
+        self.div.querySelector(`sitna-button.${self.CLASS}-btn-qr`).addEventListener("click", function (_e) {
+            const qrContainer = self.#dialogDiv.querySelector(`.${self.CLASS}-qrcode`);
+            qrContainer.innerHTML = '';
 
-                    btn.textContent = self.getLocaleString('copied');
-
-                    setTimeout(function () {
-                        btn.textContent = self.getLocaleString('copy');
-                        self.#unselectInputField();
-                    }, 1000);
-                });
-            });
-
-            self.div.querySelector('input[type=text]').addEventListener('click', function (e) {
-                self.#selectInputField(e.target);
-            });
-
-            //Enviar por e-mail
-            self.div.querySelector(`sitna-button.${self.CLASS}-btn-email`).addEventListener('click', async function (_e) {
-                const url = await self.shortenedLink();
-                if (url) {
-                    const body = encodeURIComponent(url + "\n");
-                    if (body.length > self.MAILTO_MAX_LENGTH) {
-                        self.map.toast(self.getLocaleString('urlTooLongForMailto'), { type: Consts.msgType.WARNING });
+            if (self.#dialogDiv.querySelector(`.${self.CLASS}-qr-alert`).classList.contains(Consts.classes.HIDDEN)) {
+                self.makeQRCode(qrContainer, 256).then(function (qrCodeBase64) {
+                    if (qrCodeBase64) {
+                        Util.showModal(self.#dialogDiv.querySelector(`.${self.CLASS}-qr-dialog`));
                     }
-                    window.location.href = 'mailto:?body=' + body;
-                }
-            });
+                });
+            } else {
+                Util.showModal(self.#dialogDiv.querySelector(`.${self.CLASS}-qr-dialog`));
+            }
+        });
 
-            //Generar código QR        
-            self.div.querySelector(`sitna-button.${self.CLASS}-btn-qr`).addEventListener("click", function (_e) {
-                const qrContainer = self.#dialogDiv.querySelector(`.${self.CLASS}-qrcode`);
-                qrContainer.innerHTML = '';
+        const openSocialMedia = function (win, url, process) {
+            if (url && url.trim().length > 0) {
+                win.location.href = process(url);
+            } else {
+                TC.error(self.getLocaleString('urlTooLongForShortener'));
+                win.close();
+            }
+        };
 
-                if (self.#dialogDiv.querySelector(`.${self.CLASS}-qr-alert`).classList.contains(Consts.classes.HIDDEN)) {
-                    self.makeQRCode(qrContainer, 256).then(function (qrCodeBase64) {
-                        if (qrCodeBase64) {
-                            Util.showModal(self.#dialogDiv.querySelector(`.${self.CLASS}-qr-dialog`));
-                        }
-                    });
-                } else {
-                    Util.showModal(self.#dialogDiv.querySelector(`.${self.CLASS}-qr-dialog`));
-                }
-            });
-
-            const openSocialMedia = function (win, url, process) {
-                if (url && url.trim().length > 0) {
-                    win.location.href = process(url);
-                } else {
-                    TC.error(self.getLocaleString('urlTooLongForShortener'));
-                    win.close();
-                }
-            };
-
-            //Compartir en Facebook
-            self.div.querySelector(`sitna-button.${self.CLASS}-btn-facebook`).addEventListener("click", function (_e) {
-                const w = window.open();
-                self.shortenedLink().then(function (url) {
-                    openSocialMedia(w, url, function (url) {
-                        return "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(url);
-                    });
+        //Compartir en Facebook
+        self.div.querySelector(`sitna-button.${self.CLASS}-btn-facebook`).addEventListener("click", function (_e) {
+            const w = window.open();
+            self.shortenedLink().then(function (url) {
+                openSocialMedia(w, url, function (url) {
+                    return "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(url);
                 });
             });
+        });
 
-            //Compartir en Twitter
-            self.div.querySelector(`sitna-button.${self.CLASS}-btn-twitter`).addEventListener("click", function (_e) {
-                const w = window.open();
-                self.shortenedLink().then(function (url) {
-                    openSocialMedia(w, url, function (url) {
-                        var titulo = encodeURIComponent(window.document.title ? window.document.title : "Visor API SITNA");
-                        return "https://twitter.com/intent/tweet?text=" + titulo + "&url=" + encodeURIComponent(url);
-                    });
+        //Compartir en Twitter
+        self.div.querySelector(`sitna-button.${self.CLASS}-btn-twitter`).addEventListener("click", function (_e) {
+            const w = window.open();
+            self.shortenedLink().then(function (url) {
+                openSocialMedia(w, url, function (url) {
+                    var titulo = encodeURIComponent(window.document.title ? window.document.title : "Visor API SITNA");
+                    return "https://twitter.com/intent/tweet?text=" + titulo + "&url=" + encodeURIComponent(url);
                 });
             });
+        });
 
-            //Compartir en Whatsapp
+        //Compartir en Whatsapp
+        if (Util.detectMobile()) {
+            self.div.querySelector(`sitna-button.${self.CLASS}-btn-whatsapp`).addEventListener("click", function (_e) {
+                self.shortenedLink().then(async function (url) {
+                    var waText = 'whatsapp://send?text=';
+                    if (url !== undefined) {
+                        location.href = waText + encodeURIComponent(url);
+                    } else {
+                        location.href = waText + encodeURIComponent(await self.generateLink());
+                    }
+                });
+            });
+        }
+
+        //Guardar en marcadores
+        self.div.querySelector(`sitna-button.${self.CLASS}-btn-bookmark`).addEventListener("click", async function (_e) {
+            var bookmarkURL = await self.generateLink();
+            var bookmarkTitle = document.title;
+
             if (Util.detectMobile()) {
-                self.div.querySelector(`sitna-button.${self.CLASS}-btn-whatsapp`).addEventListener("click", function (_e) {
-                    self.shortenedLink().then(async function (url) {
-                        var waText = 'whatsapp://send?text=';
-                        if (url !== undefined) {
-                            location.href = waText + encodeURIComponent(url);
-                        } else {
-                            location.href = waText + encodeURIComponent(await self.generateLink());
-                        }
-                    });
-                });
+                // Mobile browsers
+                alert(self.MOBILEFAV);
+            } else if (window.sidebar && window.sidebar.addPanel) {
+                // Firefox version < 23
+                window.sidebar.addPanel(bookmarkTitle, bookmarkURL, '');
+            } else if (window.sidebar && /Firefox/i.test(navigator.userAgent) || window.opera && window.print) {
+                // Firefox version >= 23 and Opera Hotlist                
+
+                window.location.href = bookmarkURL;
+                alert((/Mac/i.test(navigator.userAgent) ? 'Cmd' : 'Ctrl') + self.NAVALERT);
+
+            } else if (window.external && 'AddFavorite' in window.external) {
+                // IE Favorite
+                window.external.AddFavorite(bookmarkURL, bookmarkTitle);
+            } else {
+                // Other browsers (mainly WebKit - Chrome/Safari)                
+                window.location.href = bookmarkURL;
+                alert((/Mac/i.test(navigator.userAgent) ? 'Cmd' : 'Ctrl') + self.NAVALERT);
             }
+        });
 
-            //Guardar en marcadores
-            self.div.querySelector(`sitna-button.${self.CLASS}-btn-bookmark`).addEventListener("click", async function (_e) {
-                var bookmarkURL = await self.generateLink();
-                var bookmarkTitle = document.title;
-
-                if (Util.detectMobile()) {
-                    // Mobile browsers
-                    alert(self.MOBILEFAV);
-                } else if (window.sidebar && window.sidebar.addPanel) {
-                    // Firefox version < 23
-                    window.sidebar.addPanel(bookmarkTitle, bookmarkURL, '');
-                } else if (window.sidebar && /Firefox/i.test(navigator.userAgent) || window.opera && window.print) {
-                    // Firefox version >= 23 and Opera Hotlist                
-
-                    window.location.href = bookmarkURL;
-                    alert((/Mac/i.test(navigator.userAgent) ? 'Cmd' : 'Ctrl') + self.NAVALERT);
-
-                } else if (window.external && 'AddFavorite' in window.external) {
-                    // IE Favorite
-                    window.external.AddFavorite(bookmarkURL, bookmarkTitle);
-                } else {
-                    // Other browsers (mainly WebKit - Chrome/Safari)                
-                    window.location.href = bookmarkURL;
-                    alert((/Mac/i.test(navigator.userAgent) ? 'Cmd' : 'Ctrl') + self.NAVALERT);
-                }
-            });
-
-            if (Util.isFunction(callback)) {
-                callback();
-            }
-        }));
+        if (Util.isFunction(callback)) {
+            callback();
+        }
     }
 
     async #selectInputField(elm, shorten) {
