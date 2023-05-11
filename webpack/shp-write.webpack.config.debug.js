3627c40f94238a17c03087e4d789d85fde63536e
Fernando Lacunza <flacunza@users.noreply.github.com>
2023-05-11 11:33:22 +0200

    Add webpack configuration for shp-write

---
+++ webpack/shp-write.webpack.config.debug.js
@@ -0,0 +1,14 @@
+﻿const path = require('path');
+module.exports = {
+    entry: path.resolve(__dirname, '../node_modules/@aleffabricio/shp-write/index.js'),
+    output: {
+        filename: 'shp-write.js',
+        library: 'shpWrite',
+        path: path.resolve(__dirname, '../lib/shp-write/'),
+        clean: true
+    },
+    mode: 'development',
+    resolve: {
+        fallback: { "assert": false }
+    }
+};
---
+++ webpack/shp-write.webpack.config.js
@@ -0,0 +1,13 @@
+﻿const path = require('path');
+module.exports = {
+    entry: path.resolve(__dirname, '../node_modules/@aleffabricio/shp-write/index.js'),
+    output: {
+        filename: 'shp-write.min.js',
+        library: 'shpWrite',
+        path: path.resolve(__dirname, '../lib/shp-write/')
+    },
+    mode: 'production',
+    resolve: {
+        fallback: { "assert": false }
+    }
+};
