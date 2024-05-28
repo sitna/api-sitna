afc3fc8e588a67f4f3679b779b8381b8fdff5626
Fernando Lacunza <flacunza@users.noreply.github.com>
2024-05-28 11:25:14 +0200

    Refactor

--- TC/control/ScaleBar.js
+++ TC/control/ScaleBar.js
@@ -4,15 +4,6 @@ import Control from '../Control';
 TC.control = TC.control || {};
 
 class ScaleBar extends Control {
-    constructor() {
-        super(...arguments);
-        const self = this;
-        self.div.classList.add(self.CLASS);
-    }
-
-    getClassName() {
-        return 'tc-ctl-sb';
-    }
 
     render() {
         const self = this;
@@ -20,7 +11,9 @@ class ScaleBar extends Control {
             self.wrap = new TC.wrap.control.ScaleBar(self);
         }
         self.wrap.render();
-        return self._set1stRenderPromise(Promise.resolve());
+        const renderPromise = Promise.resolve();
+        self._firstRender ??= renderPromise;
+        return renderPromise;
     }
 
     async register(map) {
@@ -37,5 +30,6 @@ class ScaleBar extends Control {
     }
 }
 
+ScaleBar.prototype.CLASS = 'tc-ctl-sb';
 TC.control.ScaleBar = ScaleBar;
 export default ScaleBar;
