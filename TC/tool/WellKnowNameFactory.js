5b1a2b720889642cd2cb1f4eb42ea321dc0837a6
Fernando Lacunza <flacunza@users.noreply.github.com>
2025-02-07 15:30:14 +0100

    Add well known name factory classes

---
+++ TC/tool/WellKnowNameFactory.js
@@ -0,0 +1,85 @@
+﻿import Util from '../Util';
+import { circle, square, triangle, polygon, line } from './WellKnowNameTemplates';
+const equilateralTrianglePoints = function  (sideLength, offset = 0) {
+    // Calcular la altura del triángulo equilátero
+    const height = (Math.sqrt(3) / 2) * sideLength;
+
+    // Definir los puntos del triángulo equilátero
+    const point1 = Util.formatTemplate("{x} {y}", { x: offset, y: height + offset });
+    const point2 = Util.formatTemplate("{x} {y}", { x: sideLength + offset, y: height + offset });
+    const point3 = Util.formatTemplate("{x} {y}", { x: (sideLength / 2) + offset, y: offset });
+    return [point1, point2, point3, point1];
+}
+
+const fivePointStarPoints=function (outerRadius, offset = 0) {
+    const innerRadius = outerRadius * Math.sin(Math.PI / 10) / Math.sin(7 * Math.PI / 10);
+    const points = [];
+
+    for (let i = 0; i < 10; i++) {
+        const angle = (i * Math.PI / 5) + Math.PI / 2; // Rotar 90 grados para que una punta esté hacia arriba
+        const radius = i % 2 === 0 ? outerRadius : innerRadius;
+        points.push(Util.formatTemplate("{x} {y}", {
+            x: outerRadius + radius * Math.cos(angle) + offset,
+            y: outerRadius - radius * Math.sin(angle) + offset
+        }));
+    }
+    
+    return points;
+}
+export function crossPoints(armLength, armWidth = armLength * (1 / 3),offset=0) {
+    const halfWidth = armWidth / 2;
+    const points = [
+        Util.formatTemplate("{x} {y}", { x: -halfWidth + offset, y: -halfWidth +  offset }),
+        Util.formatTemplate("{x} {y}", { x: -halfWidth + offset, y: -armLength + offset }),
+        Util.formatTemplate("{x} {y}", { x: halfWidth + offset, y: -armLength + offset }),
+        Util.formatTemplate("{x} {y}", { x: halfWidth + offset, y: -halfWidth + offset }),
+        Util.formatTemplate("{x} {y}", { x: armLength + offset, y: -halfWidth + offset }),
+        Util.formatTemplate("{x} {y}", { x: armLength + offset, y: halfWidth + offset }),
+        Util.formatTemplate("{x} {y}", { x: halfWidth + offset, y: halfWidth + offset }),
+        Util.formatTemplate("{x} {y}", { x: halfWidth + offset, y: armLength + offset }),
+        Util.formatTemplate("{x} {y}", { x: -halfWidth + offset, y: armLength + offset }),
+        Util.formatTemplate("{x} {y}", { x: -halfWidth + offset, y: halfWidth + offset }),
+        Util.formatTemplate("{x} {y}", { x: -armLength + offset, y: halfWidth + offset }),
+        Util.formatTemplate("{x} {y}", { x: -armLength + offset, y: -halfWidth + offset })
+    ];
+
+    return points;
+}
+export function xShapePoints(armLength) {
+    const points = [
+        { x: -armLength, y: -armLength },
+        { x: -armLength / 2, y: -armLength / 2 },
+        { x: 0, y: 0 },
+        { x: armLength / 2, y: armLength / 2 },
+        { x: armLength, y: armLength },
+        { x: armLength / 2, y: -armLength / 2 },
+        { x: 0, y: 0 },
+        { x: -armLength / 2, y: armLength / 2 },
+        { x: -armLength, y: -armLength }
+    ];
+
+    return points;
+}
+
+export default class WKNFactory {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+    square(size,exceptionalSize, style, transform) {
+        return Util.formatTemplate(square, { x: (Math.max(this.width, size) - size) / 2, y: (Math.max(this.height, size) - size) / 2, width: size, height: size, style: style, transform: transform });
+    }
+    triangle(size, exceptionalSize, style, transform) {
+        return Util.formatTemplate(triangle, { points: equilateralTrianglePoints(Math.min(this.width, size), (Math.max(this.width, exceptionalSize) - size) / 2).join(","), style: style, transform: transform });
+    }
+    star(size, exceptionalSize, style, transform) {
+        return Util.formatTemplate(triangle, { points: fivePointStarPoints(size / 2, (Math.max(this.width, exceptionalSize) - size) / 2).join(","), style: style, transform: transform });
+    }
+    cross(size, exceptionalSize, style, transform) {
+        return Util.formatTemplate(triangle, { points: crossPoints(size / 2, undefined, Math.max(this.width, exceptionalSize) / 2).join(","), style: style, transform: transform });
+    }
+    circle(size, exceptionalSize, style, transform) {
+        return Util.formatTemplate(circle, { x: (Math.max(this.width, size)) / 2, y: (Math.max(this.height, size)) / 2, radius: size / 2, style: style, transform: transform });
+    }
+    
+}
---
+++ TC/tool/WellKnowNameTemplates.js
@@ -0,0 +1,7 @@
+﻿const lineTemplate = '<line x1="{x1}" y1="{y1}" x2="{x2}" y2="{y2}" {style} shape-rendering="crispEdges" >Sorry, your browser does not support inline SVG.</line>';
+const polygonTemplate = '<polygon points="0,0 {width},0 {width},{height} 0,{height} 0,0" {style} >Sorry, your browser does not support inline SVG.</polygon>';
+const circleTemplate = '<circle cx="{x}" cy="{y}" r="{radius}" {style} >Sorry, your browser does not support inline SVG.</circle>';
+const squareTemplate = '<rect  x="{x}" y="{y}" width="{width}" height="{height}" {style} {transform}>Sorry, your browser does not support inline SVG.</rect >';
+const triangleTemplate = '<polygon points="{points}" {style} >Sorry, your browser does not support inline SVG.</polygon>';
+
+export { lineTemplate as line, circleTemplate as circle, polygonTemplate as polygon, squareTemplate as square, triangleTemplate as triangle };
