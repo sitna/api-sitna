31b6dce3a14e1b973c558d59bdf3966f493b4af3
Fernando Lacunza <flacunza@users.noreply.github.com>
2025-07-30 14:54:50 +0200

    Add LayerEvent class

---
+++ SITNA/layer/LayerEvent.js
@@ -0,0 +1,22 @@
+﻿/**
+ * @class LayerEvent
+ * @memberof SITNA.layer
+ * @param {string} type - Tipo de evento.
+ * @param {object} options - Objeto de opciones del evento.
+ * @param {SITNA.layer.Layer} options.layer - Capa que provoca que se lance el evento.
+ * @param {string} [options.message] - Mensaje opcional que acompaña al evento, por ejemplo, si se ha producido un error al cargar la capa.
+ * @property {string} type - Tipo de evento.
+ * @property {SITNA.layer.Layer} layer - Capa que provoca que se lance el evento.
+ * @property {string} [message] - Mensaje opcional que acompaña al evento, por ejemplo, si se ha producido un error al cargar la capa.
+ * @see Eventos de {@link SITNA.Map}
+ */
+
+export default class LayerEvent extends Event {
+    layer;
+    newData;
+    message;
+    constructor(type, options) {
+        super(type, options);
+        Object.assign(this, options);
+    }
+}
