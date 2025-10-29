a5436ed6dce3492eb55d767dd58e00ee1d124fd9
Fernando Lacunza <flacunza@users.noreply.github.com>
2025-10-29 12:42:36 +0100

    Add SITNA.ui namespace

---
+++ SITNA/ui.js
@@ -0,0 +1,16 @@
+﻿import Component from './ui/Component.js';
+import Button from './ui/Button.js';
+import Toggle from './ui/Toggle.js';
+import Tab from './ui/Tab.js';
+
+/**
+ * Espacio de nombres de los componentes de la interfaz de usuario. Cada componente es un [Web Component](https://developer.mozilla.org/es/docs/Web/API/Web_components) que puede ser instanciado directamente en HTML o creado dinámicamente con JavaScript.
+ * @namespace SITNA.ui
+ */
+
+export default {
+    Component,
+    Button,
+    Toggle,
+    Tab,
+};
