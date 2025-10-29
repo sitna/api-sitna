44968670c9248d9816985fd2c111883f53ef0330
Fernando Lacunza <flacunza@users.noreply.github.com>
2025-10-29 12:42:11 +0100

    Add SITNA.control namespace

---
+++ SITNA/control.js
@@ -0,0 +1,125 @@
+﻿import Control from './control/Control.js';
+import Attribution from '../TC/control/Attribution.js';
+import BasemapSelector from '../TC/control/BasemapSelector.js';
+import CacheBuilder from '../TC/control/CacheBuilder.js';
+import Click from '../TC/control/Click.js';
+import Container from '../TC/control/Container.js';
+import ControlContainer from '../TC/control/ControlContainer.js';
+import Coordinates from '../TC/control/Coordinates.js';
+import DataLoader from '../TC/control/DataLoader.js';
+import Download from '../TC/control/Download.js';
+import Draw from '../TC/control/Draw.js';
+import DrawMeasureModify from '../TC/control/DrawMeasureModify.js';
+import Edit from '../TC/control/Edit.js';
+import Elevation from '../TC/control/Elevation.js';
+import ExternalWMS from '../TC/control/ExternalWMS.js';
+import FeatureDownloadDialog from '../TC/control/FeatureDownloadDialog.js';
+import FeatureInfo from '../TC/control/FeatureInfo.js';
+import FeatureTools from '../TC/control/FeatureTools.js';
+import FileEdit from '../TC/control/FileEdit.js';
+import FileImport from '../TC/control/FileImport.js';
+import FullScreen from '../TC/control/FullScreen.js';
+import Geolocation from '../TC/control/Geolocation.js';
+import GeometryFeatureInfo from '../TC/control/GeometryFeatureInfo.js';
+import LanguageSelector from '../TC/control/LanguageSelector.js';
+import LayerCatalog from '../TC/control/LayerCatalog.js';
+import Legend from '../TC/control/Legend.js';
+import LineFeatureInfo from '../TC/control/LineFeatureInfo.js';
+import LoadingIndicator from '../TC/control/LoadingIndicator.js';
+import MapContents from '../TC/control/MapContents.js';
+import MapInfo from '../TC/control/MapInfo.js';
+import Measure from '../TC/control/Measure.js';
+import Measurement from '../TC/control/Measurement.js';
+import Modify from '../TC/control/Modify.js';
+import MultiFeatureInfo from '../TC/control/MultiFeatureInfo.js';
+import NavBar from '../TC/control/NavBar.js';
+import NavBarHome from '../TC/control/NavBarHome.js';
+import OfflineMapMaker from '../TC/control/OfflineMapMaker.js';
+import OverviewMap from '../TC/control/OverviewMap.js';
+import PolygonFeatureInfo from '../TC/control/PolygonFeatureInfo.js';
+import Popup from '../TC/control/Popup.js';
+import Print from '../TC/control/Print.js';
+import PrintMap from '../TC/control/PrintMap.js';
+import ProjectionSelector from '../TC/control/ProjectionSelector.js';
+import ResultsPanel from '../TC/control/ResultsPanel.js';
+import Scale from '../TC/control/Scale.js';
+import ScaleBar from '../TC/control/ScaleBar.js';
+import ScaleSelector from '../TC/control/ScaleSelector.js';
+import Search from '../TC/control/Search.js';
+import SelectContainer from '../TC/control/SelectContainer.js';
+import Share from '../TC/control/Share.js';
+import StreetView from '../TC/control/StreetView.js';
+import SWCacheClient from '../TC/control/SWCacheClient.js';
+import TabContainer from '../TC/control/TabContainer.js';
+import ThreeD from '../TC/control/ThreeD.js';
+import TOC from '../TC/control/TOC.js';
+import WFSEdit from '../TC/control/WFSEdit.js';
+import WFSQuery from '../TC/control/WFSQuery.js';
+import WorkLayerManager from '../TC/control/WorkLayerManager.js';
+
+
+/**
+ * Espacio de nombres de los controles de la interfaz de usuario.
+ * @namespace SITNA.control
+ */
+
+export default {
+    Control,
+    Attribution,
+    BasemapSelector,
+    CacheBuilder,
+    Click,
+    Container,
+    ControlContainer,
+    Coordinates,
+    DataLoader,
+    Download,
+    Draw,
+    DrawMeasureModify,
+    Edit,
+    Elevation,
+    ExternalWMS,
+    FeatureDownloadDialog,
+    FeatureInfo,
+    FeatureTools,
+    FileEdit,
+    FileImport,
+    FullScreen,
+    Geolocation,
+    GeometryFeatureInfo,
+    LanguageSelector,
+    LayerCatalog,
+    Legend,
+    LineFeatureInfo,
+    LoadingIndicator,
+    MapContents,
+    MapInfo,
+    Measure,
+    Measurement,
+    Modify,
+    MultiFeatureInfo,
+    NavBar,
+    NavBarHome,
+    OfflineMapMaker,
+    OverviewMap,
+    PolygonFeatureInfo,
+    Popup,
+    Print,
+    PrintMap,
+    ProjectionSelector,
+    ResultsPanel,
+    Scale,
+    ScaleBar,
+    ScaleSelector,
+    Search,
+    SelectContainer,
+    Share,
+    StreetView,
+    SWCacheClient,
+    TabContainer,
+    ThreeD,
+    TOC,
+    WFSEdit,
+    WFSQuery,
+    WorkLayerManager,
+};
