624e1c3cec690e7ace60c3dd52aac6f97e42d2c8
Fernando Lacunza <flacunza@users.noreply.github.com>
2026-03-12 12:40:54 +0100

    Fix type declarations

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,19 @@
 import { Cfg, Consts, feature, layer, Map } from "./sitna";
 
+type SITNANamespace = {
+    Cfg: typeof Cfg;
+    Map: typeof Map;
+    Consts: typeof Consts;
+    feature: feature;
+    layer: layer;
+};
+
 declare global {
     interface Window {
-        SITNA: {
-            Cfg: typeof Cfg;
-            Map: typeof Map;
-            Consts: typeof Consts;
-            feature: feature;
-            layer: layer;
-        };
+        SITNA: SITNANamespace;
     }
     // For environments where globalThis is used
-    var SITNA: {
-        Cfg: typeof Cfg;
-        Map: typeof Map;
-        Consts: typeof Consts;
-        feature: feature;
-        layer: layer;
-    };
+    var SITNA: SITNANamespace;
 }
 
 export { Cfg, Consts, feature, layer, Map as default };
