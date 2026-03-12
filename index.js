2d7209067d63661c83bd2d358763c9aa4c9fdc9f
Fernando Lacunza <flacunza@users.noreply.github.com>
2026-03-12 12:44:06 +0100

    Export control and ui namespaces

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { Cfg, Map, Consts, feature, layer } from './sitna.js';
+import { Cfg, Map, Consts, feature, layer, control, ui } from './sitna.js';
 
-globalThis.SITNA = { Cfg, Map, Consts, feature, layer };
+globalThis.SITNA = { Cfg, Map, Consts, feature, layer, control, ui };
 
-export { Map as default, Cfg, Consts, feature, layer };
+export { Map as default, Cfg, Consts, feature, layer, control, ui };
